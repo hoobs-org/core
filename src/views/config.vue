e2d908bd59cda2d53496de3de033b30d4c68187d
Michael Kellsy <mkellsy@gmail.com>
2019-08-17 21:22:58 -0600

    added scope support for plugins

--- src/views/config.vue
+++ src/views/config.vue
@@ -76,9 +76,9 @@
                 <a id="plugins"></a>
                 <div v-for="(plugin, index) in plugins" :key="`${index}-platform`">
                     <div v-if="plugin.name !== 'homebridge' && platformIndex(plugin) >= 0">
-                        <h2 v-if="plugin.name === 'google-home' || plugin.name === 'homebridge-gsh'" :id="plugin.name">Google Home</h2>
+                        <h2 v-if="plugin.name === 'google-home'" :id="plugin.name">Google Home</h2>
                         <h2 v-else :id="plugin.name">{{ platformTitle(plugin) }}</h2>
-                        <p v-if="plugin.name === 'google-home' || plugin.name === 'homebridge-gsh'">
+                        <p v-if="plugin.name === 'google-home'">
                             <span>
                                 <div class="button button-primary" v-on:click="gsh">Link Account</div>
                             </span>
@@ -612,7 +612,7 @@
 
                 this.gshPopup = null;
 
-                const index = this.configuration.platforms.findIndex(p => (p.plugin_map || {}).plugin_name === "homebridge-gsh");
+                const index = this.configuration.platforms.findIndex(p => (p.plugin_map || {}).plugin_name === "google-home");
 
                 this.configuration.platforms[index].token = token;
 
