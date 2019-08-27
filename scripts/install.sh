e5fa465be7716e1da85cf9d1443223550ad04bc9
Michael Kellsy <mkellsy@gmail.com>
2019-08-27 16:25:57 -0600

    script paths

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,14 +4,14 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if command -v dnf > /dev/null; then
         echo "fedora detected"
 
-        ./fedora.sh
+        ./scripts/fedora.sh
     elif command -v yum > /dev/null; then
         echo "red hat detected"
 
-        ./redhat.sh
+        ./scripts/redhat.sh
     elif command -v apt-get > /dev/null; then
         echo "debian detected"
 
-        ./debian.sh
+        ./scripts/debian.sh
     fi
 fi
