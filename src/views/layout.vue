7d6936bb9528f9c2ed94ccfd717bcf61c20fac51
Michael Kellsy <mkellsy@gmail.com>
2019-09-24 07:00:29 -0600

    window covering missing from layout

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -379,6 +379,9 @@
                     case "motion_sensor":
                         return "motion-sensor";
 
+                    case "window_covering":
+                        return "window-covering";
+
                     case "security_system":
                         return "security-system";
 
