4f4f31fd8baf7b66c49bd779c7f9d809bb9aa10e
Michael Kellsy <mkellsy@gmail.com>
2019-11-19 13:42:05 -0700

    added some themes

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -1,10 +1,16 @@
 import "./hoobs-light/colors.css";
 import "./hoobs-dark/colors.css";
+import "./homebridge/colors.css";
+import "./legally-pink/colors.css";
 
 import HoobsLight from "./hoobs-light/theme";
 import HoobsDark from "./hoobs-dark/theme";
+import Homebridge from "./homebridge/theme";
+import LegallyPink from "./legally-pink/theme";
 
 export default {
     "hoobs-light": HoobsLight,
-    "hoobs-dark": HoobsDark
+    "hoobs-dark": HoobsDark,
+    "homebridge": Homebridge,
+    "legally-pink": LegallyPink
 };
