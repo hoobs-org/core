ad59eca54d13ca0c8f7b29dd913c0cc46abf031b
Michael Kellsy <mkellsy@gmail.com>
2019-11-19 21:04:39 -0700

    purple theme

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -1,11 +1,13 @@
 import "./hoobs-light/colors.css";
 import "./hoobs-dark/colors.css";
+import "./hazed-infused/colors.css";
 import "./throwback-blue/colors.css";
 import "./hunters-green/colors.css";
 import "./legally-pink/colors.css";
 
 import HoobsLight from "./hoobs-light/theme";
 import HoobsDark from "./hoobs-dark/theme";
+import HazedInfused from "./hazed-infused/theme";
 import ThrowbackBlue from "./throwback-blue/theme";
 import HuntersGreen from "./hunters-green/theme";
 import LegallyPink from "./legally-pink/theme";
@@ -13,6 +15,7 @@ import LegallyPink from "./legally-pink/theme";
 export default {
     "hoobs-light": HoobsLight,
     "hoobs-dark": HoobsDark,
+    "hazed-infused": HazedInfused,
     "throwback-blue": ThrowbackBlue,
     "hunters-green": HuntersGreen,
     "legally-pink": LegallyPink
